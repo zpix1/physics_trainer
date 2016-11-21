e59c358307f778333cd48eeec8aaed85f38e8de1
Ivan Baksheev <zpix-dev@list.ru>
2016-11-21 22:05:28 +0700

    bug fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,13 @@ module ApplicationHelper
         end
     end
 
+    def get_note(all,true_counter)
+        if (5*(true_counter/all)) <= 2
+            2.to_s
+        else
+            (5*(true_counter/all)).to_s
+        end
+    end
     # def note_tag(all,true_counter)
     #     ("Вы решили #{true_counter} задач из #{all}" + \
     #     "Предполагаемая оценка <br>" + \
